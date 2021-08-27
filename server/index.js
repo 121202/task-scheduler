a538c82d9c5384b57d58b196252aff3eb51b7d58
122531Takeshi <takeshi.onodera@strathmore.edu>
2021-08-27 17:28:36 +0300

    the front end

--- server/index.js
+++ server/index.js
@@ -15,10 +15,12 @@ const db=mysql.createConnection({
 });
     app.post('/create', (req,res) => {
         const task=req.body.task;
+        const timeStart=req.body.timeStart;
+        const timeEnd=req.body.timeEnd;
     
     db.query(
-        "INSERT INTO users (aab) VALUES (?)",
-        [task],
+        "INSERT INTO users2 (aab,time_start,time_end) VALUES (?,?,?)",
+        [task,timeStart,timeEnd],
         (err,result) => {
             if(err) {
                 console.log(err);
@@ -28,8 +30,8 @@ const db=mysql.createConnection({
         }
     );
 });
-app.get('/get', (req,res) => {
-    db.query("SELECT * FROM users ",(err,result) =>{
+app.get('/users', (req,res) => {
+    db.query("SELECT * FROM users2 ",(err,result) =>{
         if(err){
             console.log(err);
         }else{
@@ -39,6 +41,32 @@ app.get('/get', (req,res) => {
     })
 
 })
+app.put('/update', (req,res) => {
+    const id=req.body.id;
+    const task=req.body.task;
+    db.query(
+        "UPDATE users2 SET aab=? WHERE id=?",
+        [task,id],
+        (err,result) =>{
+            if(err){
+                console.log(err);
+            } else{
+                res.send(result);
+            }
+            }
+    );
+        
+});
+app.delete('/delete/:id', (req,res) =>{
+    const id=req.params.id;
+    db.query("DELETE FROM users2 WHERE id=?",id,(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+}); 
 
 app.listen(3001, () =>{
 console.log("HUHUUUUU");
